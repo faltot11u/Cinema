870f5138ce0775aaeebbe76f53a6a9bfb173486f
Loic Faltot <loic.faltot@gmail.com>
2020-09-30 16:27:26 +0200

    commit

--- worker.js
+++ worker.js
@@ -1,18 +1,12 @@
-const cachName = "static";
-
-self.addEventListener('install', function(event) {
+const cacheName = "static";
+self.addEventListener('install', function (event) {
     event.waitUntil(
-        caches.open(cacheName).then(function(cache) {
+        caches.open(cacheName).then(function (cache) {
             return cache.addAll(
                 [
                     'index.html',
-                    'manifest.json',
-                    'worker.js',
-                    'images/cinema128x128.png',
-                    'images/cinema144.144.png',
-                    'images/images.jfif'
                 ]
             );
         })
     );
-}); 
+});
